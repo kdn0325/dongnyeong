5fde6227f8dfb365584bf06707501beff273d071
kdn0325 <dn10003@gmail.com>
2025-07-27 01:22:02 +0900

    feat: 네비게이션 바 작성, 현재 경로에 따라 링크 스타일 동적 변경

---
+++ components/Nav.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import { Icons } from "./Icons";
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export default function Nav() {
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center space-x-4 lg:space-x-6">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Icons.logo />
+        <span className="font-bold">{siteConfig.name}</span>
+      </Link>
+      <Link
+        href={"/blog"}
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary",
+          pathname === "/blog" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Blog
+      </Link>
+      <Link
+        href={"/about"}
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary",
+          pathname === "/about" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        About
+      </Link>
+    </nav>
+  );
+}
