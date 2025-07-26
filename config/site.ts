10182792d2d0c8472651fd734fc99c0e7a48c4bc
kdn0325 <dn10003@gmail.com>
2025-07-27 01:22:18 +0900

    feat: siteConfig 객체 및 타입 정의 추가

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "dongnyeong",
+  url: "https://dongnyeong.com",
+  description: "test",
+  author: "DongNyeong",
+  link: {
+    github: "github.com/kdn0325",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
