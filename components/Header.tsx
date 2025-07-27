0839cc85d3eafb2def370df732c06fee65d75801
kdn0325 <dn10003@gmail.com>
2025-07-28 00:01:16 +0900

    feat: 헤더 컴포넌트 좌우 여백 확장 , 토글 메뉴 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,18 @@
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import React from "react";
 import { buttonVariants } from "./ui/button";
 import { Icons } from "./Icons";
 import Nav from "./Nav";
+import ModeToggle from "./ModeToggle";
 import MobileNav from "./MobileNav";
 
-export default function Header() {
+export function Header() {
   return (
-    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+    <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-10">
+      <div className="mx-auto flex h-14 items-center justify-between px-4 sm:px-6 lg:px-28">
         <Nav />
-        <div className="flex flex-1 items-center justify-end space-x-2">
+        <div className="flex items-center space-x-2">
           <nav className="flex items-center">
             <Link
               href={siteConfig.link.github}
@@ -26,9 +26,10 @@ export default function Header() {
                 )}
               >
                 <Icons.github className="h-4 w-4" />
-                <span className="sr-only">Github</span>
+                <span className="sr-only">GitHub</span>
               </div>
             </Link>
+            <ModeToggle />
             <MobileNav />
           </nav>
         </div>
