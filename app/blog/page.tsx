39aee24ef5f74ec5bbb2715ad85988373f82c40b
kdn0325 <dn10003@gmail.com>
2025-07-28 00:07:39 +0900

    feat: 블로그 페이지에 페이징 기능 및 포스트 리스트 구현

---
+++ app/blog/page.tsx
@@ -0,0 +1,78 @@
+import { posts } from "#site/content";
+import { PostItem } from "@/components/PostItem";
+import { Metadata } from "next";
+import "@/styles/mdx.css";
+import { QueryPagination } from "@/components/Query-Pagination";
+
+export const metadata: Metadata = {
+  title: "Dongnyeong's Dev Blog",
+  description:
+    "단순히 내가 배운걸 다시 잊어버리지 않기 위해 그냥 적어놓습니다.",
+};
+
+const POSTS_PER_PAGE = 5;
+
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function BlogPage({ searchParams }: Props) {
+  const currentPage = Number(searchParams?.page) || 1;
+  //post.published true인 포스트만 필터링하고 최신순 정렬
+  const sortedPosts = posts
+    .filter((post) => post.published)
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  // 전체 페이지 수 계산
+  const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+  // 현재 페이지에 표시할 포스트 자르기
+  const displayPosts = sortedPosts.slice(
+    POSTS_PER_PAGE * (currentPage - 1),
+    POSTS_PER_PAGE * currentPage
+  );
+
+  return (
+    <main className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 py-10">
+      {/* 헤더 섹션 */}
+      <section className="mb-10">
+        <div className="space-y-2 text-center md:text-left">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Dongnyeong's Dev Blog
+          </h1>
+          <p className="text-muted-foreground text-lg">
+            단순히 내가 배운걸 다시 잊어버리지 않기 위해 그냥 적어놓습니다.
+          </p>
+        </div>
+      </section>
+
+      {/* 포스트 리스트 섹션 */}
+      <section>
+        <hr className="mb-6" />
+        {displayPosts?.length > 0 ? (
+          <ul className="space-y-6">
+            {displayPosts.map((post) => (
+              <li key={post.slug}>
+                <PostItem
+                  slug={post.slug}
+                  date={post.date}
+                  title={post.title}
+                  description={post.description}
+                />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-center text-muted-foreground">
+            Nothing to see here yet.
+          </p>
+        )}
+
+        {/* 페이지네이션 */}
+        <div className="mt-10 flex justify-end">
+          <QueryPagination totalPages={totalPages} />
+        </div>
+      </section>
+    </main>
+  );
+}
