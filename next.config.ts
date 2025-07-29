8fe4210cc0898f56a699c01b43891243e4bedd43
kdn0325 <dn10003@gmail.com>
2025-07-29 14:29:06 +0900

    chore: 웹팩 플러그인 등록 / Velite 빌드 트리거

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,35 @@
 import { build } from "velite";
+import type { NextConfig } from "next";
+import type { Compiler } from "webpack";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// Velite 빌드를 트리거하는 커스텀 Webpack
+class VeliteWebpackPlugin {
+  static started = false;
+
+  apply(compiler: Compiler) {
+    //  Webpack을 총 3번 실행
+    // - 서버용(nodejs 런타임) 1회
+    // - 서버용(edge 런타임) 1회
+    // - 클라이언트용 1회
+    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
+      if (VeliteWebpackPlugin.started) return; // 중복 실행 방지
+      VeliteWebpackPlugin.started = true;
+
+      const dev = compiler.options.mode === "development";
+      // 개발 환경에서는 watch 모드, 프로덕션에선 clean 후 빌드
+      await build({ watch: dev, clean: !dev });
+    });
+  }
+}
+
+const nextConfig: NextConfig = {
   images: {
     domains: ["github.com", "ko.reactjs.org", "nextjs.org"],
   },
   webpack: (config) => {
-    config.plugins.push(new VeliteWebpackPlugin());
+    config.plugins.push(new VeliteWebpackPlugin()); // Webpack 커스텀 플러그인 등록
     return config;
   },
 };
 
 export default nextConfig;
-
-class VeliteWebpackPlugin {
-  static started = false;
-  apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
-      if (VeliteWebpackPlugin.started) return;
-      VeliteWebpackPlugin.started = true;
-      const dev = compiler.options.mode === "development";
-      await build({ watch: dev, clean: !dev });
-    });
-  }
-}
