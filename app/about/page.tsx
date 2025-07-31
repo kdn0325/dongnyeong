538003877b87c164994b1864cf20f13db3334deb
kdn0325 <dn10003@gmail.com>
2025-07-31 16:12:28 +0900

    style: 현재 테마에 따라 색상 변경

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import GithubActivity from "@/components/GithubActivity";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { siteConfig } from "@/config/site";
 import { Metadata } from "next";
@@ -10,18 +11,14 @@ export const metadata: Metadata = {
 
 export default async function AboutPage() {
   return (
-    <main className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 py-10">
-      {/* Header */}
+    <main className="mx-auto max-w-4xl px-4 sm:px-6 md:px-12 py-10">
       <div className="mb-10">
         <h1 className="text-4xl lg:text-5xl font-black tracking-tight">
           About Me
         </h1>
         <hr className="mt-4 border-muted" />
       </div>
-
-      {/* Profile */}
       <div className="flex flex-col md:flex-row gap-10">
-        {/* Avatar */}
         <div className="flex flex-col items-center md:items-start w-full md:w-1/3">
           <Avatar className="h-40 w-40">
             <AvatarImage src="/avatar.png" alt={siteConfig.author} />
@@ -31,23 +28,24 @@ export default async function AboutPage() {
             {siteConfig.author}
           </h2>
           <p className="text-muted-foreground text-center md:text-left">
-            Software Developer
+            Front-End Developer
           </p>
         </div>
-
-        {/* Introduction */}
         <div className="flex-1 space-y-6">
           <p className="text-muted-foreground text-lg leading-relaxed text-justify">
             안녕하세요.{" "}
-            <span className="text-amber-500 font-semibold">
+            <span className="font-semibold text-[#1565c0] dark:text-[#ff9800]">
               최상의 사용자 경험(UX)
             </span>
-            과
-            <span className="text-amber-500 font-semibold">
+            과{" "}
+            <span className="font-semibold text-[#1565c0] dark:text-[#ff9800]">
               더 나은 개발자 경험(DX)
             </span>
             을 고려하는 모바일 엔지니어{" "}
-            <span className=" font-semibold">김동녕</span>입니다.
+            <span className="font-semibold text-[#1565c0] dark:text-[#ff9800]">
+              김동녕
+            </span>
+            입니다.
           </p>
 
           <ul className="list-disc pl-5 space-y-3 text-base leading-relaxed text-muted-foreground">
@@ -65,6 +63,12 @@ export default async function AboutPage() {
           </ul>
         </div>
       </div>
+      <section className="mt-16">
+        <h2 className="text-2xl font-bold mb-6">GitHub Activity</h2>
+        <div className="rounded-md border p-4 sm:p-6 overflow-x-auto">
+          <GithubActivity />
+        </div>
+      </section>
     </main>
   );
 }
