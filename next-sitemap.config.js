277aa167876ff1fc3a9e1e5f53be12a517beee97
kdn0325 <dn10003@gmail.com>
2025-08-02 01:05:47 +0900

    feat: ci/cd 파이프라인에서 xml 생성되도록 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.dongnyeong.com",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 7000,
+  exclude: [],
+};
